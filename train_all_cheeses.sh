13271260d2a4af2922c5a28516fc2c041571db56
Adib <154505489+Axidix@users.noreply.github.com>
2024-05-24 01:10:42 +0200

    error on path

--- train_all_cheeses.sh
+++ train_all_cheeses.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the base directory
-baseDir="/home/adib4/Documents/Travail/X/MODAL DL/cheese_classification_challenge"
+baseDir=/Data/mellah.adib/cheese_classification_challenge"
 
 # Define the list of cheese names
 cheeseNames=(
