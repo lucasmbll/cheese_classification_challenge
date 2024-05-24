eeb85cf47d30bcb909b92d86feb5fbd3741d79f4
Adib <154505489+Axidix@users.noreply.github.com>
2024-05-24 10:00:56 +0200

    Update train_all_cheeses.sh

--- train_all_cheeses.sh
+++ train_all_cheeses.sh
@@ -31,7 +31,7 @@ for cheeseName in "${cheeseNames[@]}"; do
 
     # Construct the command
     # command="accelerate launch '$baseDir/diffusers/examples/dreambooth/train_dreambooth_lora.py' --pretrained_model_name_or_path='runwayml/stable-diffusion-v1-5' --output_dir='$outputDir' --checkpointing_steps=50 --mixed_precision='fp16' --instance_data_dir='$instanceDir' --instance_prompt='a photo of a $cheeseName cheese' --use_8bit_adam --gradient_checkpointing --with_prior_preservation --prior_loss_weight=1.0 --class_data_dir='$classDataDir' --class_prompt='a photo of a $cheeseName cheese'"
-    command="accelerate launch '$baseDir/diffusers/examples/dreambooth/train_dreambooth_lora.py' --pretrained_model_name_or_path='runwayml/stable-diffusion-v1-5' --output_dir='$outputDir' --checkpointing_steps=50 --mixed_precision='fp16' --instance_data_dir='$instanceDir' --instance_prompt='a photo of a $cheeseName cheese' --use_8bit_adam --gradient_checkpointing
+    command="accelerate launch '$baseDir/diffusers/examples/dreambooth/train_dreambooth_lora.py' --pretrained_model_name_or_path='runwayml/stable-diffusion-v1-5' --output_dir='$outputDir' --checkpointing_steps=50 --mixed_precision='fp16' --instance_data_dir='$instanceDir' --instance_prompt='a photo of a $cheeseName cheese' --use_8bit_adam --gradient_checkpointing"
 
     # Execute the command
     echo "Training model for $cheeseName..."
