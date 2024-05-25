468e8b64b7f851a60750bc27a9003806ceedf47f
Adib <154505489+Axidix@users.noreply.github.com>
2024-05-25 06:12:41 +0200

    Revert "SD with Dreambooth Lucas"
    This reverts commit 94c43ec0dcbb63d26ea58d0d1ff9ab7f66caf4d0.

--- train_all_cheeses.sh
+++ train_all_cheeses.sh
@@ -1,52 +1,22 @@
 #!/bin/bash
 
+# Define the base directory
+baseDir="/Data/mellah.adib/cheese_classification_challenge"
+
 # Define the list of cheese names
 cheeseNames=(
-    "BRIE DE MELUN"
-    "CAMEMBERT"
-    "EPOISSES"
-    "FOURME D’AMBERT"
-    "RACLETTE"
-    "MORBIER"
-    "SAINT-NECTAIRE"
-    "POULIGNY SAINT- PIERRE"
-    "ROQUEFORT"
-    "COMTÉ"
-    "CHÈVRE"
-    "PECORINO"
-    "NEUFCHATEL"
-    "CHEDDAR"
-    "BÛCHETTE DE CHÈVRE"
-    "PARMESAN"
-    "SAINT- FÉLICIEN"
-    "MONT D’OR"
-    "STILTON"
-    "SCARMOZA"
-    "CABECOU"
-    "BEAUFORT"
-    "MUNSTER"
-    "CHABICHOU"
-    "TOMME DE VACHE"
-    "REBLOCHON"
-    "EMMENTAL"
-    "FETA"
-    "OSSAU- IRATY"
-    "MIMOLETTE"
-    "MAROILLES"
-    "GRUYÈRE"
-    "MOTHAIS"
     "VACHERIN"
     "MOZZARELLA"
     "TÊTE DE MOINES"
     "FROMAGE FRAIS"
 )
 
-
 # Iterate over each cheese in the list
 for cheeseName in "${cheeseNames[@]}"; do
     # Set the environment variables
-    instanceDir="/users/eleves-b/2022/lucas.mebille/Documents/cheese_challenge/dataset/train_db/$cheeseName"
-    outputDir="/users/eleves-b/2022/lucas.mebille/Documents/cheese_challenge/db_models_lucas/$cheeseName"
+    instanceDir="$baseDir/dataset/val/$cheeseName"
+    outputDir="$baseDir/db_models/$cheeseName"
+    classDataDir="$baseDir/class_data/$cheeseName"
 
     # Create the output directory if it doesn't exist
     if [ ! -d "$outputDir" ]; then
@@ -54,8 +24,8 @@ for cheeseName in "${cheeseNames[@]}"; do
     fi
 
     # Construct the command
-    command="accelerate launch /users/eleves-b/2022/lucas.mebille/Documents/cheese_challenge/diffusers/examples/dreambooth/train_dreambooth.py --pretrained_model_name_or_path=runwayml/stable-diffusion-v1-5 --output_dir=\"$outputDir\" \
-            --checkpointing_steps=50 --mixed_precision=fp16 --instance_data_dir=\"$instanceDir\" --instance_prompt=\"$cheeseName cheese\" --use_8bit_adam \
+    command="accelerate launch $baseDir/diffusers/examples/dreambooth/train_dreambooth.py --pretrained_model_name_or_path=runwayml/stable-diffusion-v1-5 --output_dir=\"$outputDir\" \
+            --checkpointing_steps=50 --mixed_precision=fp16 --instance_data_dir=\"$instanceDir\" --instance_prompt=\"a photo of a $cheeseName cheese\" --use_8bit_adam \
             --gradient_checkpointing --snr_gamma=5.0 --train_batch_size=1 --gradient_accumulation_steps=1 --learning_rate=2e-6 --lr_scheduler=constant --lr_warmup_steps=0 --max_train_steps=400"
 
     # Execute the command
