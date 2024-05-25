bb921cc0c70faa757fff31e48e0f4d4dbd15fbbf
Adib <154505489+Axidix@users.noreply.github.com>
2024-05-25 17:48:40 +0200

    param for new db train

--- train_all_cheeses.sh
+++ train_all_cheeses.sh
@@ -5,18 +5,50 @@ baseDir="/Data/mellah.adib/cheese_classification_challenge"
 
 # Define the list of cheese names
 cheeseNames=(
-    "VACHERIN"
-    "MOZZARELLA"
-    "TÊTE DE MOINES"
+    "BRIE DE MELUN",
+    "CAMEMBERT",
+    "EPOISSES",
+    "FOURME D’AMBERT",
+    "RACLETTE",
+    "MORBIER",
+    "SAINT-NECTAIRE",
+    "POULIGNY SAINT- PIERRE",
+    "ROQUEFORT",
+    "COMTÉ",
+    "CHÈVRE",
+    "PECORINO",
+    "NEUFCHATEL",
+    "CHEDDAR",
+    "BÛCHETTE DE CHÈVRE",
+    "PARMESAN",
+    "SAINT- FÉLICIEN",
+    "MONT D’OR",
+    "STILTON",
+    "SCARMOZA",
+    "CABECOU",
+    "BEAUFORT",
+    "MUNSTER",
+    "CHABICHOU",
+    "TOMME DE VACHE",
+    "REBLOCHON",
+    "EMMENTAL",
+    "FETA",
+    "OSSAU- IRATY",
+    "MIMOLETTE",
+    "MAROILLES",
+    "GRUYÈRE",
+    "MOTHAIS",
+    "VACHERIN",
+    "MOZZARELLA",
+    "TÊTE DE MOINES",
     "FROMAGE FRAIS"
 )
 
 # Iterate over each cheese in the list
 for cheeseName in "${cheeseNames[@]}"; do
     # Set the environment variables
-    instanceDir="$baseDir/dataset/val/$cheeseName"
-    outputDir="$baseDir/db_models/$cheeseName"
-    classDataDir="$baseDir/class_data/$cheeseName"
+    instanceDir="$baseDir/dataset/val_sorted/$cheeseName"
+    outputDir="$baseDir/db_models/val_sorted/$cheeseName"
 
     # Create the output directory if it doesn't exist
     if [ ! -d "$outputDir" ]; then
@@ -25,8 +57,8 @@ for cheeseName in "${cheeseNames[@]}"; do
 
     # Construct the command
     command="accelerate launch $baseDir/diffusers/examples/dreambooth/train_dreambooth.py --pretrained_model_name_or_path=runwayml/stable-diffusion-v1-5 --output_dir=\"$outputDir\" \
-            --checkpointing_steps=50 --mixed_precision=fp16 --instance_data_dir=\"$instanceDir\" --instance_prompt=\"a photo of a $cheeseName cheese\" --use_8bit_adam \
-            --gradient_checkpointing --snr_gamma=5.0 --train_batch_size=1 --gradient_accumulation_steps=1 --learning_rate=2e-6 --lr_scheduler=constant --lr_warmup_steps=0 --max_train_steps=400"
+            --checkpointing_steps=600 --mixed_precision=fp16 --instance_data_dir=\"$instanceDir\" --instance_prompt=\"a photo of a $cheeseName cheese\" --use_8bit_adam \
+            --gradient_checkpointing --snr_gamma=5.0 --train_batch_size=1 --gradient_accumulation_steps=1 --learning_rate=2e-6 --lr_scheduler=constant --lr_warmup_steps=0 --max_train_steps=800"
 
     # Execute the command
     echo "Training model for $cheeseName..."
